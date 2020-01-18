c2cf76ac3cdd0395f110b94b2142c0ca56041835
Simba <pryde.geh@gmail.com>
2020-01-17 22:27:10 -0500

    react installed

---
+++ .Trash/React-Portfolio-master/src/components/ContactUs.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+export default class ContactUs extends Component {
+  render() {
+    let resumeData = this.props.resumeData;
+    return (
+      <section id="contact">
+        <div className="row section-head">
+          <div className="ten columns">
+            <p className="lead">
+              Feel free to contact me for any work or suggestions below
+              </p>
+          </div>
+        </div>
+        <div className="row">
+          <aside className="eigth columns footer-widgets">
+            <div className="widget">
+              <h4>Linked In :</h4>
+              <h5> {resumeData.linkedinId} </h5>
+              <h4> Email: </h4>
+              <h5>{resumeData.email}</h5>
+            </div>
+          </aside>
+        </div>
+      </section >
+    );
+  }
+}
---
+++ .Trash/React-Portfolio-master/src/resumeData.js
@@ -0,0 +1,119 @@
+let resumeData = {
+  "imagebaseurl": "https://simbapryd3.github.io/",
+  "name": "Pryde Geh",
+  "role": "Software Developer and Data Analyst",
+  "linkedinId": "linkedin.com/in/pryde-geh-989389194",
+  "skypeid": "Your skypeid",
+  "roleDescription": " Or even better you can say D1 Athlete turned FullStack Developer. I still like to stay active. Health and fitness will always be my first love. I also enjoy playing video games (mostly role-playing). If you want to learn more, please dont hesistate to rach out to me. Love Life and Laugh as much as possible!",
+  "socialLinks": [
+    {
+      "name": "linkedin",
+      "url": "http://linkedin.com/in/pryde-geh-989389194",
+      "className": "fa fa-linkedin"
+    },
+    {
+      "name": "github",
+      "url": "http://github.com/simbapryd3",
+      "className": "fa fa-github"
+    }
+  ],
+  "aboutme": "Experienced Technician with a demonstrated history of working in the consumer electronics industry and skilled in Databases, Equipment Maintenance, and Object-Oriented Programming. Strong, creative developer who values learning all aspects of technology. Driven by the array of ventures the software developer role provides and how software and hardware testing improve human efficiency and the quality of life..",
+  "address": "Columbus, Ohio",
+  "website": "https://simbapryd3.github.io/",
+  "email": "pryde.geh@gmail.com",
+  "education": [
+    {
+      "UniversityName": "Ohio University",
+      "specialization": "Exercise Physiology and Psycology",
+      "MonthOfPassing": "May",
+      "YearOfPassing": "2015",
+      "Achievements": "Men's Wrestilning Team, ROTC Bobcat Battalion Graduate, Fitness Odyssey Health Coach"
+    },
+    {
+      "UniversityName": "University of Cincinnati",
+      "specialization": "Masters of Health Informatics",
+      "MonthOfPassing": "Dec",
+      "YearOfPassing": "2018",
+      "Achievements": "Database Management Certification, Software Development Certification"
+    },
+    {
+      "UniversityName": "WeCanCodeIt",
+      "specialization": "Full-Stack JavaScript Developer",
+      "MonthOfPassing": "Dec",
+      "YearOfPassing": "2019",
+    }
+  ],
+  "work": [
+    {
+      "CompanyName": "Ohio Army National Guard",
+      "specialization": "Data Analyst",
+      "MonthOfLeaving": "Sept",
+      "YearOfLeaving": "2019",
+      "Achievements": "Implemented a maintenance program that helped the Ohio Army National Guard receive over 500,000 dollars back through three quarters in 2019. Conducted on-site inspection of 15 maintenance facilities which documented changes needed for the state to improve future utilization. Analyzed and evaluated standard operating procedures weekly by defining specific vulnerabilities for each maintenance facility."
+    },
+    {
+      "CompanyName": "University of Cincinnati Hospital (UC Health)",
+      "specialization": "Health Informatics Intern",
+      "MonthOfLeaving": "Dec",
+      "YearOfLeaving": "2018",
+      "Achievements": "Increased the utilization from 41% to 72% of Hospital rooms in the Endoscopy department by developing a composite outcome metric that was delivered from UC’s internal Epic database for patients under perioperative care. Gained a clear understanding of organizational procedures and protocols for the access of health information. Presented the financial concerns and utilization through illustration via the Power BI dashboard."
+    }
+  ],
+  "skillsDescription": "Your skills here",
+  "skills": [
+    {
+      "skillname": "JavaScript"
+    },
+    {
+      "skillname": "HTML5"
+    },
+    {
+      "skillname": "CSS"
+    },
+    {
+      "skillname": "Reactjs"
+    },
+    {
+      "skillname": "SQL"
+    }
+  ],
+  "portfolio": [
+    {
+      "name": "project1",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    },
+    {
+      "name": "project2",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project.jpg"
+    },
+    {
+      "name": "project3",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/project2.png"
+    },
+    {
+      "name": "project4",
+      "description": "mobileapp",
+      "imgurl": "images/portfolio/phone.jpg"
+    }
+  ],
+  "testimonials": [
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "This is a sample testimonial",
+      "name": "Some technical guy"
+    },
+    {
+      "description": "Where you going LT?",
+      "name": "unknown"
+    }
+
+  ]
+}
+
+export default resumeData
